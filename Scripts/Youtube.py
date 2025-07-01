3be75b36b73ae5f467fe9247320a6fff5b5a5fed
Sebastian Ramos <sebastian.ramos70@myhunter.cuny.edu>
2025-07-01 18:51:06 +0000

    Added Gemini API, Style checking yaml scripts

--- Scripts/Youtube.py
+++ Scripts/Youtube.py
@@ -12,8 +12,13 @@ List the top-trending YouTube videos (no OAuth needed).
 """
 
 from googleapiclient.discovery import build
+from dotenv import load_dotenv
+import os
 
-API_KEY = ""      # ←—— paste the key you created in Google Cloud
+load_dotenv()                # pulls key-value pairs out of .env into the real environment
+api_key = os.getenv("YOUTUBE_CLIENT_API")
+
+API_KEY = api_key # ←—— paste the key you created in Google Cloud
 API_SERVICE_NAME = "youtube"
 API_VERSION = "v3"
 
@@ -30,6 +35,12 @@ def main() -> None:
         regionCode="US",              # change to e.g. "TW" or "GB" if you want
         maxResults=10                 # optional; default 5, max 50
     )
+    #We found you cannot filter for captions through a top trending access point get request. Must first get the list of trending
+    #Then search (Search video endpoint) them, and make sure to only return the videos that have captions enabled
+    #2. If it’s somebody else’s video (most cases) → use an unofficial transcript puller
+    # Because the official API bars you from other people’s caption files, developers lean on the player caption end-point.
+    # A tiny library called youtube-transcript-api wraps that for you:
+
 
     response = request.execute()
 
